aaf3ecfc70ed6f628741f3100ad6672830210920
OllieRumbol <otgb97@gmail.com>
2021-03-14 14:39:48 +0000

    Adding models

---
+++ DraughtsGameAPIModels/Service/AvailableBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DraughtsGameAPIModels.Service
+{
+    public class AvailableBoard
+    {
+
+        public int[,] Board { get; set; }
+
+        public int CurrentHeight { get; set; }
+
+        public int CurrentWidth { get; set; }
+
+        public int NextHeight { get; set; }
+
+        public int NextWidth { get; set; }
+
+        public List<Take> Takes { get; set; }
+    }
+}
