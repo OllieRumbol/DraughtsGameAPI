3303321c38100e96ffb35efde7d23048b5632e65
OllieRumbol <otgb97@gmail.com>
2021-03-16 13:16:58 +0000

    Check and find move helpers

---
+++ DraughtsGameAPIService/Helpers/CheckMove.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DraughtsGameAPIService.Helpers
+{
+    public class CheckMove
+    {
+        public static bool checkMoveUpLeft(int[,] board, int height, int width)
+        {
+            try
+            {
+                if (board[height - 1, width - 1] == 5 || board[height - 1, width - 1] == 6)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch { return false; }
+        }
+
+        public static bool checkMoveUpRight(int[,] board, int height, int width)
+        {
+            try
+            {
+                if (board[height - 1, width + 1] == 5 || board[height - 1, width + 1] == 6)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch { return false; }
+        }
+
+        public static bool checkMoveDownLeft(int[,] board, int height, int width)
+        {
+            try
+            {
+                if (board[height + 1, width - 1] == 5 || board[height + 1, width - 1] == 6)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch { return false; }
+        }
+
+        public static bool checkMoveDownRight(int[,] board, int height, int width)
+        {
+            try
+            {
+                if (board[height + 1, width + 1] == 5 || board[height + 1, width + 1] == 6)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch { return false; }
+        }
+
+        public static Tree checkTakeDown(int[,] board, int i, int j, int[] playerToTake, Tree tree)
+        {
+            try
+            {
+                //left
+                if (Array.IndexOf(playerToTake, board[i + 1, j - 1]) > -1)
+                {
+                    if (board[i + 2, j - 2] == 5 || board[i + 2, j - 2] == 6)
+                    {
+                        tree.Left = new Tree(new TreeTake
+                        {
+                            TakeHeight = i + 1,
+                            TakeWidth = j - 1,
+                            NextHeight = i + 2,
+                            NextWidth = j - 2
+                        });
+                        checkTakeDown(board, i + 2, j - 2, playerToTake, tree.Left);
+                    }
+                }
+            }
+            catch { }
+
+            try
+            {
+                //Right
+                if (Array.IndexOf(playerToTake, board[i + 1, j + 1]) > -1)
+                {
+                    if (board[i + 2, j + 2] == 5 || board[i + 2, j + 2] == 6)
+                    {
+                        tree.Right = new Tree(new TreeTake
+                        {
+                            TakeHeight = i + 1,
+                            TakeWidth = j + 1,
+                            NextHeight = i + 2,
+                            NextWidth = j + 2
+                        });
+                        checkTakeDown(board, i + 2, j + 2, playerToTake, tree.Right);
+                    }
+                }
+            }
+            catch { }
+
+            return tree;
+        }
+
+        public static Tree checkTakeUp(int[,] board, int i, int j, int[] playerToTake, Tree tree)
+        {
+            try
+            {
+                //left
+                if (Array.IndexOf(playerToTake, board[i - 1, j - 1]) > -1)
+                {
+                    if (board[i - 2, j - 2] == 5 || board[i - 2, j - 2] == 6)
+                    {
+                        tree.Left = new Tree(new TreeTake
+                        {
+                            TakeHeight = i - 1,
+                            TakeWidth = j - 1,
+                            NextHeight = i - 2,
+                            NextWidth = j - 2
+                        });
+                        checkTakeUp(board, i - 2, j - 2, playerToTake, tree.Left);
+                    }
+                }
+            }
+            catch { }
+
+            try
+            {
+                //Right
+                if (Array.IndexOf(playerToTake, board[i - 1, j + 1]) > -1)
+                {
+                    if (board[i - 2, j + 2] == 5 || board[i - 2, j + 2] == 6)
+                    {
+                        tree.Right = new Tree(new TreeTake
+                        {
+                            TakeHeight = i - 1,
+                            TakeWidth = j + 1,
+                            NextHeight = i - 2,
+                            NextWidth = j + 2
+                        });
+                        checkTakeUp(board, i - 2, j + 2, playerToTake, tree.Right);
+                    }
+                }
+            }
+            catch { }
+
+            return tree;
+        }
+
+        public static KingTree checkKingTake(int[,] board, int preHeight, int preWidth, int height, int width, int[] playerToTake, KingTree tree)
+        {
+            try
+            {
+                //Down left
+                if (Array.IndexOf(playerToTake, board[height + 1, width - 1]) > -1)
+                {
+                    if (board[height + 2, width - 2] == 5 || board[height + 2, width - 2] == 6)
+                    {
+                        if (preHeight != height + 2 || preWidth != width - 2)
+                        {
+                            tree.DownLeft = new KingTree(new TreeTake
+                            {
+                                TakeHeight = height + 1,
+                                TakeWidth = width - 1,
+                                NextHeight = height + 2,
+                                NextWidth = width - 2
+                            });
+                            checkKingTake(board, height, width, height + 2, width - 2, playerToTake, tree.DownLeft);
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            try
+            {
+                //Down Right
+                if (Array.IndexOf(playerToTake, board[height + 1, width + 1]) > -1)
+                {
+                    if (board[height + 2, width + 2] == 5 || board[height + 2, width + 2] == 6)
+                    {
+                        if (preHeight != height + 2 || preWidth != width + 2)
+                        {
+                            tree.DownRight = new KingTree(new TreeTake
+                            {
+                                TakeHeight = height + 1,
+                                TakeWidth = width + 1,
+                                NextHeight = height + 2,
+                                NextWidth = width + 2
+                            });
+                            checkKingTake(board, height, width, height + 2, width + 2, playerToTake, tree.DownRight);
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            try
+            {
+                //Up left
+                if (Array.IndexOf(playerToTake, board[height - 1, width - 1]) > -1)
+                {
+                    if (board[height - 2, width - 2] == 5 || board[height - 2, width - 2] == 6)
+                    {
+                        if (preHeight != height - 2 || preWidth != width - 2)
+                        {
+                            tree.UpLeft = new KingTree(new TreeTake
+                            {
+                                TakeHeight = height - 1,
+                                TakeWidth = width - 1,
+                                NextHeight = height - 2,
+                                NextWidth = width - 2
+                            });
+                            checkKingTake(board, height, width, height - 2, width - 2, playerToTake, tree.UpLeft);
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            try
+            {
+                //Up Right
+                if (Array.IndexOf(playerToTake, board[height - 1, width + 1]) > -1)
+                {
+                    if (board[height - 2, width + 2] == 5 || board[height - 2, width + 2] == 6)
+                    {
+                        if (preHeight != height - 2 || preWidth != width + 2)
+                        {
+                            tree.UpRight = new KingTree(new TreeTake
+                            {
+                                TakeHeight = height - 1,
+                                TakeWidth = width + 1,
+                                NextHeight = height - 2,
+                                NextWidth = width + 2
+                            });
+                            checkKingTake(board, height, width, height - 2, width + 2, playerToTake, tree.UpRight);
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            return tree;
+        }
+    }
+}
---
+++ DraughtsGameAPIService/Helpers/FindMove.cs
@@ -0,0 +1,326 @@
+﻿using DraughtsGameAPIModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DraughtsGameAPIService.Helpers
+{
+    public class FindMove
+    {
+        public static List<NextMove> FindAvailableMoves(int[,] board, int player)
+        {
+            List<NextMove> results = new List<NextMove>();
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                // Skip over pieces that cant be player
+                for (int j = 1 - (i % 2); j < board.GetLength(1); j += 2)
+                {
+                    int piece = board[i, j];
+                    if (piece == 1 && player == 1)
+                    {
+                        if (CheckMove.checkMoveUpLeft(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i - 1,
+                                NextWidth = j - 1
+                            });
+                        }
+                        if (CheckMove.checkMoveUpRight(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i - 1,
+                                NextWidth = j + 1
+                            });
+                        }
+
+                        Tree resultTree = CheckMove.checkTakeUp(board, i, j, new int[] { 2 }, new Tree(new TreeTake
+                        {
+                            CurrentHeight = i,
+                            CurrentWidth = j,
+                        }));
+                        if (resultTree.Left != null || resultTree.Right != null)
+                        {
+                            List<List<TreeTake>> treeArray = Tree.TreeToArray(resultTree);
+                            results.AddRange(ProcessTreeArray(treeArray));
+                        }
+                    }
+                    else if (piece == 2 && player == 2)
+                    {
+                        if (CheckMove.checkMoveDownLeft(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i + 1,
+                                NextWidth = j - 1
+                            });
+                        }
+                        if (CheckMove.checkMoveDownRight(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i + 1,
+                                NextWidth = j + 1
+                            });
+                        }
+
+                        Tree resultTree = CheckMove.checkTakeDown(board, i, j, new int[] { 1 }, new Tree(new TreeTake
+                        {
+                            CurrentHeight = i,
+                            CurrentWidth = j,
+                        }));
+                        if (resultTree.Left != null || resultTree.Right != null)
+                        {
+                            List<List<TreeTake>> treeArray = Tree.TreeToArray(resultTree);
+                            results.AddRange(ProcessTreeArray(treeArray));
+                        }
+                    }
+                    else if ((piece == 3 && player == 1) || (piece == 4 && player == 2))
+                    {
+                        if (CheckMove.checkMoveUpLeft(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i - 1,
+                                NextWidth = j - 1
+                            });
+                        }
+                        if (CheckMove.checkMoveUpRight(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i - 1,
+                                NextWidth = j + 1
+                            });
+                        }
+                        if (CheckMove.checkMoveDownLeft(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i + 1,
+                                NextWidth = j - 1
+                            });
+                        }
+                        if (CheckMove.checkMoveDownRight(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i + 1,
+                                NextWidth = j + 1
+                            });
+                        }
+
+                        int[] countersToTake = piece == 3 ? new int[] { 2, 4 } : new int[] { 1, 3 };
+                        KingTree resultTree = CheckMove.checkKingTake(board, i, j, i, j, countersToTake, new KingTree(new TreeTake
+                        {
+                            CurrentHeight = i,
+                            CurrentWidth = j,
+                        }));
+                        if (resultTree.DownLeft != null || resultTree.DownRight != null || resultTree.UpLeft != null || resultTree.UpRight != null)
+                        {
+                            List<List<TreeTake>> treeArray = KingTree.KingTreeToArray(resultTree);
+                            results.AddRange(ProcessTreeArray(treeArray));
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public static List<NextMove> FindAvailableMoves(int[,] board)
+        {
+            List<NextMove> results = new List<NextMove>();
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                // Skip over pieces that cant be player
+                for (int j = 1 - (i % 2); j < board.GetLength(1); j += 2)
+                {
+                    int piece = board[i, j];
+                    if (piece == 1)
+                    {
+                        if (CheckMove.checkMoveUpLeft(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i - 1,
+                                NextWidth = j - 1
+                            });
+                        }
+                        if (CheckMove.checkMoveUpRight(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i - 1,
+                                NextWidth = j + 1
+                            });
+                        }
+
+                        Tree resultTree = CheckMove.checkTakeUp(board, i, j, new int[] { 2 }, new Tree(new TreeTake
+                        {
+                            CurrentHeight = i,
+                            CurrentWidth = j,
+                        }));
+                        if (resultTree.Left != null || resultTree.Right != null)
+                        {
+                            List<List<TreeTake>> treeArray = Tree.TreeToArray(resultTree);
+                            results.AddRange(ProcessTreeArray(treeArray));
+                        }
+                    }
+                    else if (piece == 2)
+                    {
+                        if (CheckMove.checkMoveDownLeft(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i + 1,
+                                NextWidth = j - 1
+                            });
+                        }
+                        if (CheckMove.checkMoveDownRight(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i + 1,
+                                NextWidth = j + 1
+                            });
+                        }
+
+                        Tree resultTree = CheckMove.checkTakeDown(board, i, j, new int[] { 1 }, new Tree(new TreeTake
+                        {
+                            CurrentHeight = i,
+                            CurrentWidth = j,
+                        }));
+                        if (resultTree.Left != null || resultTree.Right != null)
+                        {
+                            List<List<TreeTake>> treeArray = Tree.TreeToArray(resultTree);
+                            results.AddRange(ProcessTreeArray(treeArray));
+                        }
+                    }
+                    else if ((piece == 3) || (piece == 4))
+                    {
+                        if (CheckMove.checkMoveUpLeft(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i - 1,
+                                NextWidth = j - 1
+                            });
+                        }
+                        if (CheckMove.checkMoveUpRight(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i - 1,
+                                NextWidth = j + 1
+                            });
+                        }
+                        if (CheckMove.checkMoveDownLeft(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i + 1,
+                                NextWidth = j - 1
+                            });
+                        }
+                        if (CheckMove.checkMoveDownRight(board, i, j))
+                        {
+                            results.Add(new NextMove
+                            {
+                                CurrentHeight = i,
+                                CurrentWidth = j,
+                                NextHeight = i + 1,
+                                NextWidth = j + 1
+                            });
+                        }
+
+                        int[] countersToTake = piece == 3 ? new int[] { 2, 4 } : new int[] { 1, 3 };
+                        KingTree resultTree = CheckMove.checkKingTake(board, i, j, i, j, countersToTake, new KingTree(new TreeTake
+                        {
+                            CurrentHeight = i,
+                            CurrentWidth = j,
+                        }));
+                        if (resultTree.DownLeft != null || resultTree.DownRight != null || resultTree.UpLeft != null || resultTree.UpRight != null)
+                        {
+                            List<List<TreeTake>> treeArray = KingTree.KingTreeToArray(resultTree);
+                            results.AddRange(ProcessTreeArray(treeArray));
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public static List<NextMove> ProcessTreeArray(List<List<TreeTake>> treeArray)
+        {
+            List<NextMove> takeMoves = new List<NextMove>();
+
+            foreach(List<TreeTake> takes in treeArray)
+            {
+                takeMoves.AddRange(processTakeMove(takes));
+            }
+
+            return takeMoves;
+        }
+
+        public static List<NextMove> processTakeMove(List<TreeTake> takeMoves)
+        {
+            List<NextMove> results = new List<NextMove>();
+
+            List<Take> takes = new List<Take>();
+
+            foreach(TreeTake take in takeMoves.Skip(1))
+            {
+                takes.Add(new Take
+                {
+                    Height = take.TakeHeight,
+                    Width = take.TakeWidth
+                });
+
+                results.Add(new NextMove
+                {
+                    CurrentHeight = takeMoves[0].CurrentHeight,
+                    CurrentWidth = takeMoves[0].CurrentWidth,
+                    NextHeight = take.NextHeight,
+                    NextWidth = take.NextWidth,
+                    Takes = takes
+                });
+            }
+
+            return results;
+        }
+    }
+}
